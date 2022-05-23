c8ecd48286e70c7da86d334d70a3604003cf35f7
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-05-23 07:30:41 +0000

    block / Test constructor / toString()

--- dev-test.js
+++ dev-test.js
@@ -1,3 +1,9 @@
 
-const firstName = "Florin";
-console.log("Hallo, " + firstName);
+
+// Import
+const Block = require("./block");
+
+/***  Test der Block-Funktionalität  */
+const block = new Block("aktuelle Zeit","hash1","hash2","block data");
+console.log(block.toString());
+
